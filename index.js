3e9c382af60ff1a638941ec3f3f8adfb086abad2
Rupayan <154427141+Rupayan0001@users.noreply.github.com>
2025-02-08 00:22:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
+app.set("trust proxy", 1);
 app.use((req, res, next) => {
   if (req.headers["x-forwarded-proto"] === "http") {
     return res.redirect(`https://${req.headers.host}${req.url}`);

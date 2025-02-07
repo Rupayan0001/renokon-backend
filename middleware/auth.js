a3ced23069c99a29d51bcd315d53b39f3b171cdd
Rupayan <154427141+Rupayan0001@users.noreply.github.com>
2025-02-08 00:15:22 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,7 @@ import Usermodel from "../model/user.model.js";
 export const auth = async (req, res, next) => {
   try {
     let token = req.cookies?.token || req.headers["authorization"]?.split(" ")[1];
+     console.log("token", token);
     if (!token) {
       return res.status(401).json({ message: "You are not logged in" });
     }

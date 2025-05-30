3fda7a885c56fd7651cbc1114f019492854a9efd
Rupayan <rupayan.team@gmail.com>
2025-05-30 10:11:15 +0530

    .

--- routes/ecommerce.route.js
+++ routes/ecommerce.route.js
@@ -3,9 +3,13 @@ const router = express.Router();
 import { auth } from "../middleware/auth.js";
 import { getProducts, getProductById } from "../controller/ecommerce.controller.js";
 import upload from "../lib/multerConfiguration.js";
+import { whisperApi } from "../lib/whisperAPI.js";
+import multer from "multer";
+const uploadAudio = multer({ dest: "uploads/" });
 
 router.get("/getProducts", getProducts);
 router.get("/getProductById/:id", getProductById);
+router.post("/transcribe", uploadAudio.single("audio"), whisperApi);
 router.use(auth);
 
 // router.post("/create", upload.single("image"), createProduct);
